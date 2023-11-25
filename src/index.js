fb42c5160d0514faa320155620a36824af1b6764
Nonyedim <chinonye.dimanochie@gmail.com>
2023-11-24 19:00:37 -0500

    replaced emoji with icon and added two lines selecting an empty icon element and injecting the appropriate image using icon url

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ function refreshWeather(response) {
   let feelsLikeElement = document.querySelector("#feels-like");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
