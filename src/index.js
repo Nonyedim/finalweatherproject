219a01cc64a86a7099a470c2650e7e3154cbae09
Nonyedim <chinonye.dimanochie@gmail.com>
2023-12-02 18:06:01 -0500

    replaced the demi content with real and current data by integrating the Api

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "6369030e9f5o2da37b9324412t6c0373";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -64,22 +71,27 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-       <div class="weather-forecast-date">${day}</div>
-         <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" class="weather-forecast-icon">
+       <div class="weather-forecast-date">${formatDay(day.time)}</div>
+         <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
           <div class="weather-forecast-temeratures">
-          <span class="weather-forecast-temperature-max">↑18°</span>
-          <span class="weather-forecast-temperature-min">↓12°</span>
+          <span class="weather-forecast-temperature-max">↑${Math.round(
+            day.temperature.maximum
+          )}°</span>
+          <span class="weather-forecast-temperature-min">↓${Math.round(
+            day.temperature.minimum
+          )}°</span>
        </div>
       </div>
       `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
